c79f7a9c8f86b6dee16cd22e42d4555117635631
starydv7 <pawanstar72926@gmail.comm>
2022-09-20 23:55:06 +0530

    matrial ui

--- src/components/CounterPage.jsx
+++ src/components/CounterPage.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react'
 
 const CounterPage = () => {
-    const [data, setData] = useState([]);
+    const [countrydata, setCountryData] = useState([]);
     const fetchData = () => {
         fetch("https://restcountries.com/v3.1/all")
             .then((res) => res.json())
