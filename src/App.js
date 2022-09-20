3320ffa431ca8f3695355c085055413d64cf49d6
starydv7 <pawanstar72926@gmail.comm>
2022-09-21 02:40:19 +0530

    searching minor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import CounterPage from './components/CounterPage';
 
+import { useState } from "react";
+
 function App() {
+   const [countries, setCountries] = useState([]);
   return (
     <div className="App">
      <CounterPage/>
+    
     </div>
   );
 }
--- src/components/CounterPage.jsx
+++ src/components/CounterPage.jsx
@@ -5,7 +5,9 @@ import { AppContext } from './Context';
 import { useNavigate } from "react-router-dom";
 const CounterPage = () => {
     const [countrydata, setCountrydata] = useState([]);
-     const { setNewdata, newdata } = useContext(AppContext);
+    const { setNewdata, newdata } = useContext(AppContext);
+    const [countryList, setCountryList] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
     const navigate = useNavigate();
     const fetchData = () => {
         fetch("https://restcountries.com/v3.1/all")
@@ -50,50 +52,94 @@ const CounterPage = () => {
             })
             }
     }
+    function handleChange(e) {
+      setSearchTerm(e.target.value);
+      const list = countrydata.filter((p) =>
+        p.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      setCountrydata(list);
+    }
     
   return (
+    <div>
       <div>
-       <div>
-        <h3>Sort by Population :- <span>
-            <select onChange={sorthandle}  >
-                <option value="/">Sort by Population</option>
-                <option value="desc">High to Low</option>
-                <option value="asc">Low to High</option>
-               
+        <h3>
+          Sort by Population :-{" "}
+          <span>
+            <select onChange={sorthandle}>
+              <option value="/">Sort by Population</option>
+              <option value="desc">High to Low</option>
+              <option value="asc">Low to High</option>
             </select>
-        </span></h3>
-       </div>
-       <div>
-        <h3>Filter by Regision :- <span>
-            <select onChange={filterhandle} >
-                <option value="empty">Select Regision </option>
-          <option value="Africa">Africa</option>
-          <option value="Americas">Americas</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
+          </span>
+        </h3>
+      </div>
+      <div>
+        <h3>
+          Filter by Regision :-{" "}
+          <span>
+            <select onChange={filterhandle}>
+              <option value="empty">Select Regision </option>
+              <option value="Africa">Africa</option>
+              <option value="Americas">Americas</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
             </select>
-        </span></h3> 
-       </div>
-
-    <div className={styles.pawan}>
-        {
-        countrydata && countrydata.map((el,i)=>{
-            return <div key={i}>
-          <img className={styles.img} src={el.coatOfArms?.svg ? el.coatOfArms?.svg :"https://preview.redd.it/5mr5jn1kd8111.png?auto=webp&s=887ce4f9569f6d7abb7cd3412015640a841354e6"} alt="" />
-          <h2>{el.name?.common}</h2>
-         <h3>Population: <span>{el.population}</span></h3>
-         <h3>Regision: <span>{el.region}</span></h3>
-         <h3>Capital: <span>{el.capital}</span></h3>
-         <button className={styles.submit} onClick={()=>{
-            setNewdata(el)
-            navigate("/details")
-
-         }}>More details</button>
+          </span>
+        </h3>
+      </div>
+      <label>find countries</label>
+      <input value={searchTerm} onChange={handleChange} />
+      <br />
+      {countrydata.length > 1 &&
+        countrydata.length < 10 &&
+        countrydata.map((list) => {
+          return (
+            <div key={list.name} style={{ display: "flex" }}>
+              <p>{list.name}</p>{" "}
+              <button onClick={() => setCountrydata([list])}>Show</button>
             </div>
-        })
-        }
-    </div>
+          );
+        })}
+
+      <div className={styles.pawan}>
+        {countrydata &&
+          countrydata.map((el, i) => {
+            return (
+              <div key={i}>
+                <img
+                  className={styles.img}
+                  src={
+                    el.coatOfArms?.svg
+                      ? el.coatOfArms?.svg
+                      : "https://preview.redd.it/5mr5jn1kd8111.png?auto=webp&s=887ce4f9569f6d7abb7cd3412015640a841354e6"
+                  }
+                  alt=""
+                />
+                <h2>{el.name?.common}</h2>
+                <h3>
+                  Population: <span>{el.population}</span>
+                </h3>
+                <h3>
+                  Regision: <span>{el.region}</span>
+                </h3>
+                <h3>
+                  Capital: <span>{el.capital}</span>
+                </h3>
+                <button
+                  className={styles.submit}
+                  onClick={() => {
+                    setNewdata(el);
+                    navigate("/details");
+                  }}
+                >
+                  More details
+                </button>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
